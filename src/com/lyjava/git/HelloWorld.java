f1731490c8cbe25723a3e3e2da0ab54871c579f8
lyjava <lyjava@www.com>
2022-08-04 21:27:45 +0800

    push test

--- src/com/lyjava/git/HelloWorld.java
+++ src/com/lyjava/git/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("hello wordl2");
         System.out.println("hello world3");
+        System.out.println("push test");
     }
 }
